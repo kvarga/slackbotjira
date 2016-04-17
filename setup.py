fa383106d33841e48e822576388ab6e85223d6c2
Kyle Varga <kyle.varga@mutualmobile.com>
2016-04-17 15:45:49 -0500

    fix requirements and increment version

--- setup.py
+++ setup.py
@@ -1,20 +1,14 @@
 from setuptools import setup
-from pip.req import parse_requirements
-
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements('requirements.txt', session=False)
-
-# reqs is a list of requirement
-# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
-reqs = [str(ir.req) for ir in install_reqs]
 
 setup(name='slackbotjira',
-      version='0.2',
+      version='0.3',
       description='Slackbot JIRA Plugin',
       url='tbd',
       author='Kyle Varga',
       author_email='slackbot-jira@kylevarga.com',
-      install_requires=reqs,
+      install_requires=[
+        'jira',
+      ],
       license='MIT',
       packages=['slackbotjira'],
       zip_safe=False)
